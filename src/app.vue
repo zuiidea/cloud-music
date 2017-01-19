8d1351b500f372dec57396a97069653336722c90
zuiidea <zuiiidea@gmail.com>
2017-01-19 21:04:39 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -29,30 +29,6 @@
       </f7-view>
     </f7-panel>
 
-    <!-- Right Panel -->
-    <f7-panel right cover layout="dark">
-      <f7-view id="right-panel-view" navbar-through :dynamic-navbar="true">
-        <f7-navbar title="Right Panel" sliding></f7-navbar>
-        <f7-pages>
-          <f7-page>
-            <f7-block>
-              <p>Right panel content goes here</p>
-            </f7-block>
-            <f7-block-title>Load page in panel</f7-block-title>
-            <f7-list>
-              <f7-list-item link="/about/" title="About"></f7-list-item>
-              <f7-list-item link="/form/" title="Form"></f7-list-item>
-            </f7-list>
-            <f7-block-title>Load page in main view</f7-block-title>
-            <f7-list>
-              <f7-list-item link="/about/" title="About" link-view="#main-view" link-close-panel></f7-list-item>
-              <f7-list-item link="/form/" title="Form" link-view="#main-view" link-close-panel></f7-list-item>
-            </f7-list>
-          </f7-page>
-        </f7-pages>
-      </f7-view>
-    </f7-panel>
-
     <!-- Main Views -->
     <f7-views>
       <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
@@ -62,9 +38,6 @@
             <f7-link icon="icon-bars" open-panel="left"></f7-link>
           </f7-nav-left>
           <f7-nav-center sliding>Framework7</f7-nav-center>
-          <f7-nav-right>
-            <f7-link icon="icon-bars" open-panel="right"></f7-link>
-          </f7-nav-right>
         </f7-navbar>
         <!-- Pages -->
         <f7-pages>
@@ -105,50 +78,6 @@
         </f7-pages>
       </f7-view>
     </f7-views>
-
-    <!-- Popup -->
-    <f7-popup id="popup">
-      <f7-view navbar-fixed>
-        <f7-pages>
-          <f7-page>
-            <f7-navbar title="Popup">
-              <f7-nav-right>
-                <f7-link close-popup>Close</f7-link>
-              </f7-nav-right>
-            </f7-navbar>
-            <f7-block>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.</f7-block>
-          </f7-page>
-        </f7-pages>
-      </f7-view>
-    </f7-popup>
-
-    <!-- Login Screen -->
-    <f7-login-screen id="login-screen">
-      <f7-view>
-        <f7-pages>
-          <f7-page login-screen>
-            <f7-login-screen-title>Login</f7-login-screen-title>
-            <f7-list form>
-              <f7-list-item>
-                <f7-label>Username</f7-label>
-                <f7-input name="username" placeholder="Username" type="text"></f7-input>
-              </f7-list-item>
-              <f7-list-item>
-                <f7-label>Password</f7-label>
-                <f7-input name="password" type="password" placeholder="Password"></f7-input>
-              </f7-list-item>
-            </f7-list>
-            <f7-list>
-              <f7-list-button title="Sign In" close-login-screen></f7-list-button>
-              <f7-list-label>
-                <p>Click Sign In to close Login Screen</p>
-              </f7-list-label>
-            </f7-list>
-          </f7-page>
-        </f7-pages>
-      </f7-view>
-    </f7-login-screen>
-
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Framework7 from 'framework7'
 import Framework7Vue from 'framework7-vue'
 import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
 import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
-import AppStyles from './css/app.css'
+import AppStyles from './css/app.less'
 import Routes from './routes.js'
 import App from './app'
 
