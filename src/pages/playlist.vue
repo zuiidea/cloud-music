3a770ee0cb51ed5c4e008ffb2a0f51194462d302
zuiidea <zuiiidea@gmail.com>
2017-01-28 23:10:01 +0800

    playlist

--- src/pages/playlist.vue
+++ src/pages/playlist.vue
@@ -6,7 +6,7 @@
        <f7-link icon="icon-bars" open-panel="left"></f7-link>
      </f7-nav-right>
    </f7-navbar>
-   <f7-list media-list>
+   <f7-list media-list class="play-list">
     <f7-list-item link title="死结" subtitle="李玖哲 - 不，完美" media="1"></f7-list-item>
     <f7-list-item title="Por Una Cabeza" subtitle="Thomas Newman - Scent Of A Woman" media="2"></f7-list-item>
    </f7-list>
